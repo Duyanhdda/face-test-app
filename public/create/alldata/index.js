d1ffb461f20be6b780f705aff79b7051e9183555
Duyanhdda <anh.haduyanh@hcmut.edu.vn>
2021-12-27 01:15:54 +0700

    update

---
+++ public/create/alldata/index.js
@@ -0,0 +1,24 @@
+$(".btn-search").click(function (e) {
+
+    var url = $(this).data("url");
+
+    // $("#HistoryStudentModal input[name='id']").val(id);
+    console.log(url);
+    $('.dataimg').attr("src", url);
+    $('#HistoryStudentModal').modal('show');
+})
+$('#tab-users').DataTable({
+    "responsive": true, "lengthChange": true, "autoWidth": true, "searching": true, "bPaginate": false, "dom": "lfrti",
+    language: {
+        url: "//cdn.datatables.net/plug-ins/1.10.25/i18n/Vietnamese.json"
+    } ,
+    "scrollY":        "550px",
+    "columns": [
+        { "width": "20%" },
+        { "width": "20%" },
+        { "width": "20%" },
+        { "width": "20%" },
+        { "width": "40%" },
+        { "width": "20%" },
+      ]
+    })
