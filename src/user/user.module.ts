ad92d2cce20e87ec89ac11733e61e6713bf17c44
Duyanhdda <anh.haduyanh@hcmut.edu.vn>
2021-12-27 01:28:14 +0700

    hi

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import {MongooseModule  } from "@nestjs/mongoose";
 import { UserController } from "./user.controller";
-import { userSchema } from "./User.model";
+import { userSchema } from "./user.model";
 
 
 import { UserService } from "./user.service";
